71d20c02e81bb4e3268fc30a6d0150dc6195aa73
Nigel Bartholomeusz <nigelbartholomeusz@gmail.com>
2020-09-29 12:14:19 +1000

    Update script.js
    Add modal code to JS

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-// capture all items with a class of artwork
-const pic = document.querySelectorAll(".artwork");
-// each time an item with class of rtwork is clicked change class to headinOn
-pic.forEach((pic) => {
-	pic.addEventListener("mouseover", () => {
-		pic.classList.toggle("headingOn");
-	});
+const cardButtons = document.querySelectorAll(".card button");
+const modalOuter = document.querySelector(".modal-outer");
+const modalInner = document.querySelector(".modal-inner");
+
+function handleCardButtonClick(event) {
+	const button = event.currentTarget;
+	const card = button.closest(".card");
+	// Grab the image src
+	const imgSrc = card.querySelector("img").src;
+
+	const desc = card.dataset.description;
+	const name = card.querySelector("h2").textContent;
+
+	// populate the modal with the new info
+	modalInner.innerHTML = `
+    <img width="600" height="600" src="${imgSrc.replace(
+			"200",
+			"600"
+		)}" alt="${name}"/>
+    <h2>${desc}</h2>
+  `;
+	// show the modal
+	modalOuter.classList.add("open");
+}
+
+cardButtons.forEach((button) =>
+	button.addEventListener("click", handleCardButtonClick)
+);
+
+function closeModal() {
+	modalOuter.classList.remove("open");
+}
+
+modalOuter.addEventListener("click", function (event) {
+	const isOutside = !event.target.closest(".modal-inner");
+	if (isOutside) {
+		closeModal();
+	}
+});
+
+window.addEventListener("keydown", (event) => {
+	console.log(event);
+	if (event.key === "Escape") {
+		closeModal();
+	}
 });
